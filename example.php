d8084c847a5eece5039a5dc416819a76b0364104
Ali Mihandoost <i@ali.md>
2012-12-29 01:26:38 +0330

    testing

--- example.php
+++ example.php
@@ -1,32 +1,21 @@
 <?php
 
-/* ===================================
- * Author: Nazarkin Roman
- * -----------------------------------
- * Contacts:
- * email - roman@nazarkin.su
- * icq - 642971062
- * skype - roman444ik
- * -----------------------------------
- * GitHub:
- * https://github.com/NazarkinRoman
- * ===================================
-*/
-
 require_once('cache.class.php');
 
-$cache = new MicroCache($_SERVER["REQUEST_URI"]);
+$cache = new MicroCache();
 
 if(@$_GET['act'] == 'page_without_caching')
   $cache->cache_on = false; // disable cache
 
-else if(@$_GET['act'] == 'page_with_1minute_cache')
-  $cache->lifetime = 60; // set the cache lifetime to 60 seconds
+$cache->lifetime = 5; // set the cache lifetime to 60 seconds
+$cache->memcache_compressed=true;
 
 if($cache->check()) die($cache->out());
 else $cache->start();
 
 // your code here
+
+echo ('testing ...');
 
 $cache->end();
 ?>
